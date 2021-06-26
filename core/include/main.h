d18ace731dc3655e03bbcb4eb1673a6c2c810c53
OmegaKi113r <omegaki113r@gmail.com>
2021-06-26 17:57:35 +0530

    initial commit

---
+++ core/include/main.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+extern "C" void app_main(void);
---
+++ core/src/main.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file main.c
+ * @author Om3gaki113r
+ * @brief 
+ * @version 0.1
+ * @date 2021-06-12
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "main.h"
+
+
+void app_main(void)
+{
+
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+}
